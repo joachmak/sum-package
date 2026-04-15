87ca2f05fb0ccb8e376bbf6b636809ee8bf183f6
joachmak <joachimmaksim@gmail.com>
2026-04-15 07:40:11 +0200

    refactor: konverter til esm for tree shaking

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-function sum(a, b) {
+export function sum(a, b) {
   return a + b;
 }
 
-module.exports = { sum };
+export function subtract(a, b) {
+  return a - b;
+}
 
-
-
-
-
-
+export function divide(a, b) {
+  return a  / b;
+}
